3915e5cab004e61063094b0a4778d449796f72eb
Robbe Haegeman <robbe@haegeman.org>
2024-12-10 14:50:00 +0100

    feat(Poc): create first draft of simple wrapper
    Currently doesn't keep track of secondary resources, just manages the API object

--- operators/poc-secondary-resource-management/src/crd.rs
+++ operators/poc-secondary-resource-management/src/crd.rs
@@ -1,8 +1,12 @@
+use crate::error::{Error, Result};
+use kube::{
+    api::{DynamicObject, PostParams},
+    Api, Client,
+};
 use kube_derive::CustomResource;
 use schemars::JsonSchema;
-use serde::{Deserialize, Serialize};
-
-// #[serde(skip_serializing_if = "Option::is_none")]
+use serde::{de::DeserializeOwned, Deserialize, Serialize};
+use std::fmt::Debug;
 
 #[derive(CustomResource, Debug, Serialize, Deserialize, Default, Clone, JsonSchema)]
 #[kube(group = "poc.sec.res", version = "v1", kind = "Database", namespaced)]
@@ -16,4 +20,37 @@ pub struct DatabaseSpec {
 
 #[derive(Debug, Serialize, Deserialize, Default, Clone, JsonSchema)]
 #[serde(rename_all = "camelCase")]
-pub struct DatabaseStatus {}
+pub struct DatabaseStatus {
+    // Note: these will have an OwnerReference as well, but there is no way in Kubernetes to query them
+    // apart from getting all resources and filtering on the value
+    #[serde(skip)]
+    pub sec_recs: Vec<DynamicObject>,
+}
+
+impl PrimaryResource for Database {}
+
+pub(crate) trait PrimaryResource: kube::ResourceExt {
+    async fn create_secondary<
+        T: kube::Resource<Scope = k8s_openapi::NamespaceResourceScope>
+            + Clone
+            + Debug
+            + Serialize
+            + DeserializeOwned,
+    >(
+        &self,
+        client: Client,
+        pp: &mut PostParams,
+        data: &T,
+    ) -> Result<T>
+    where
+        <T as kube::Resource>::DynamicType: Default,
+    {
+        let secondary_api: Api<T> =
+            Api::namespaced(client, &self.namespace().unwrap_or(String::from("default")));
+
+        secondary_api
+            .create(pp, data)
+            .await
+            .map_err(Error::KubeError)
+    }
+}
