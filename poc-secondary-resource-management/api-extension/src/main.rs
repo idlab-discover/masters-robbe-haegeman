4ba050be940570d416d77fe3dcccefaf1d3c9545
Robbe Haegeman <robbe@haegeman.org>
2025-03-16 22:03:07 +0100

    feat(multiple kinds): add basic path parsing to server

--- poc-secondary-resource-management/api-extension/src/main.rs
+++ poc-secondary-resource-management/api-extension/src/main.rs
@@ -1,11 +1,20 @@
-use axum::{Router, routing::get};
+use axum::{Json, Router, extract::Path, routing::get};
+use serde_json::{Value, json};
+use tower::ServiceBuilder;
+use tower_http::trace::TraceLayer;
 
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/{user_id}", get(basic_handler))
+        .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()));
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+async fn basic_handler(Path(user_id): Path<u32>) -> Json<Value> {
+    Json(json!({"user_id": user_id}))
 }
