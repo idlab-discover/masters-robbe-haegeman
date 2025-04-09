d58024bb999659b80a9fcaafedb079c2638d65d8
Robbe Haegeman <robbe@haegeman.org>
2025-04-09 19:41:54 +0200

    feat(multi kinds): fetch all resource types

--- poc-secondary-resource-management/api-extension/src/resources.rs
+++ poc-secondary-resource-management/api-extension/src/resources.rs
@@ -1,14 +1,18 @@
 use anyhow::Context;
 use axum::{Json, extract::Path};
-use k8s_openapi::api::apps;
-use kube::{Api, Client, ResourceExt};
+use kube::{
+    Api, Client, Discovery, ResourceExt,
+    api::DynamicObject,
+    discovery::{Scope, verbs},
+};
 use serde_json::Value;
+use tracing::info;
 
 use crate::{API_VERSION, GROUP, error::Result};
 
 pub(crate) struct MockResource {
     name: String,
-    sec_res: Vec<String>,
+    sec_res: Vec<DynamicObject>,
 }
 
 impl MockResource {
@@ -32,27 +36,48 @@ impl MockResource {
 pub(crate) async fn get_primary_resource(
     Path((namespace, name)): Path<(String, String)>,
 ) -> Result<Json<Value>> {
-    let client = Client::try_default().await.expect("Client Creation Error");
-
     let mut prim_res = MockResource {
         name,
         sec_res: vec![],
     };
 
-    let deploy = Api::<apps::v1::Deployment>::namespaced(client, &namespace)
-        .get(&prim_res.name)
+    // Source: https://github.com/kube-rs/kube/blob/d171d2620e8ad82235230fe589bbea7c9306963d/examples/dynamic_api.rs
+    let client = Client::try_default()
         .await
-        .with_context(|| {
-            format!(
-                "The resource \"{}\" in ns \"{}\" does not exist",
-                prim_res.name, namespace
-            )
-        })?;
+        .context("Client Creation Error")?;
+    let discovery = Discovery::new(client.clone())
+        .run()
+        .await
+        .context("Discovery Creation Error")?;
 
-    prim_res.sec_res.push(deploy.name_any());
+    for group in discovery.groups() {
+        for (ar, caps) in group.recommended_resources() {
+            if !caps.supports_operation(verbs::LIST) || caps.scope != Scope::Namespaced {
+                continue;
+            }
+            let api: Api<DynamicObject> = Api::namespaced_with(client.clone(), &namespace, &ar);
+
+            info!(
+                "({}) {}/{} : {}",
+                namespace,
+                group.name(),
+                ar.version,
+                ar.kind
+            );
+
+            let list = api
+                .list(&Default::default())
+                .await
+                .with_context(|| format!("No secondary resource of kind \"{}\" found", ar.kind))?;
+
+            prim_res.sec_res.extend(list);
+        }
+    }
+
     Ok(Json(serde_json::json!({
         "apiVersion": MockResource::api_version(),
         "kind": MockResource::kind(),
-        "sec_res": prim_res.sec_res,
+        "name": prim_res.name,
+        "sec_res": prim_res.sec_res.iter().map(|obj| obj.name_any()).collect::<Vec<String>>(),
     })))
 }
