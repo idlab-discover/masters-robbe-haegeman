37aa59af52a7a8dff567b7c929258488ed92265e
Robbe Haegeman <robbe@haegeman.org>
2025-05-22 19:19:55 +0200

    feat(benchmark): request memory

--- poc/benchmark/src/main.rs
+++ poc/benchmark/src/main.rs
@@ -1,4 +1,4 @@
-use std::time::SystemTime;
+use std::{mem::ManuallyDrop, time::SystemTime};
 
 use case::{Case, append_case_to_file, create_test_secrets};
 use crd::Database;
@@ -8,12 +8,14 @@ use kube::{
     api::{ListParams, ObjectMeta, PostParams},
 };
 use kube_primary::PrimaryResourceExt;
+use sysinfo::System;
 
 pub mod case;
 pub mod crd;
 
 #[tokio::main]
 async fn main() {
+    let mut sys = System::new_all();
     let client = Client::try_default().await.unwrap();
     let db = Database {
         metadata: ObjectMeta {
@@ -31,7 +33,7 @@ async fn main() {
         None => db_api.create(&PostParams::default(), &db).await.unwrap(),
     };
 
-    create_test_secrets(client.clone(), &mut db, 5).await;
+    // create_test_secrets(client.clone(), &mut db, 5).await;
 
     let mut case = Case::new(5, 1);
 
@@ -49,10 +51,17 @@ async fn main() {
 
         let start = SystemTime::now();
 
+        print!("Before");
+        get_memory_usage(&mut sys);
         let db_api: Api<Database> = Api::namespaced(client.clone(), "poc-testing");
         let db = db_api.get(&db.name_any()).await;
-
+        let mut db = ManuallyDrop::new(db);
+        print!("After");
+        get_memory_usage(&mut sys);
         assert!(db.is_ok());
+        unsafe {
+            ManuallyDrop::drop(&mut db);
+        }
 
         let secret_api: Api<Secret> = Api::namespaced(client.clone(), "poc-testing");
         let secrets = secret_api.list(&ListParams::default()).await;
@@ -65,4 +74,13 @@ async fn main() {
         }
     }
     append_case_to_file(&case, "./result.jsonl").unwrap();
+}
+
+fn get_memory_usage(sys: &mut System) {
+    sys.refresh_all();
+    if let Some(process) = sys.process(sysinfo::get_current_pid().unwrap()) {
+        println!("Memory usage: {} KB", process.memory());
+    } else {
+        println!("Process not found");
+    }
 }
